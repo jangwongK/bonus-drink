e81c0a8cd8fb0e18fe581ccc2b04b6a6dca19852
jangwongK <masao31536@gmail.com>
2021-02-19 08:29:06 +0900

    answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,43 +1,34 @@
-loop do
-  $total = 0
+class BonusDrink
 
-  def bottle
-    a = gets.to_i
-    b = 0
-    c = 0
-
-    $buy = a
-    $total = 0
-
-    loop do
-      b = a/3
-      c = a%3
-
-      if a<3
-        $total = $total + 3*b
-        a = b + c
-      break
-      else
-        $total = $total + 3*b
-        a = b + c
-      end
-
-    end
-
-    $total = $total + a
-
-    if $total == 0
-      puts "If you bought #{$buy} bottles,You can't drink a bottle.\n"
-    elsif $buy == 1
-      puts "If you bought #{$buy} bottles,You can drink one bottle.\n"
-    else
-      puts "If you bought #{$buy} bottles,You can drink #{$total} bottles.\n"
-    end
-
+  def self.total_count_for(amount)
+    new(amount).total_count
   end
 
-  print "How many bottles did you buy at first?(type number)    "
+  def initialize(a, b = 0, c = 0, total = 0)
+    @a = a
+    @b = b
+    @c = c
+    @buy = a
+    @total = total
+  end
 
-  bottle
+  def total_count
+    loop do
+      @b = @a/3
+      @c = @a%3
+
+      if @a<3
+        @total = @total + 3*@b
+        @a = @b + @c
+        @total = @total + @a
+      break
+      else
+        @total = @total + 3*@b
+        @a = @b + @c
+      end
+    end
+    @total
+  end
+
 
 end
