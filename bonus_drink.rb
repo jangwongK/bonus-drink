f5c1b079d37c8bcbb20c48410f1cca84dc6c5c4e
jangwongK <masao31536@gmail.com>
2021-02-18 13:27:46 +0900

    jangwongK

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,43 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    0
+loop do
+  $total = 0
+
+  def bottle
+    a = gets.to_i
+    b = 0
+    c = 0
+
+    $buy = a
+    $total = 0
+
+    loop do
+      b = a/3
+      c = a%3
+
+      if a<3
+        $total = $total + 3*b
+        a = b + c
+      break
+      else
+        $total = $total + 3*b
+        a = b + c
+      end
+
+    end
+
+    $total = $total + a
+
+    if $total == 0
+      puts "If you bought #{$buy} bottles,You can't drink a bottle.\n"
+    elsif $buy == 1
+      puts "If you bought #{$buy} bottles,You can drink one bottle.\n"
+    else
+      puts "If you bought #{$buy} bottles,You can drink #{$total} bottles.\n"
+    end
+
   end
+
+  print "How many bottles did you buy at first?(type number)    "
+
+  bottle
+
 end
